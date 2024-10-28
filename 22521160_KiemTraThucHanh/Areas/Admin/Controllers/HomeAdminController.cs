d01c6f2daa797301a03c0ba5fe959d109131e0ff
PhuongHo105 <22521160@gm.uit.edu.vn>
2024-10-28 20:41:45 +0700

    Update

---
+++ 22521160_KiemTraThucHanh/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,32 @@
+﻿using _22521160_KiemTraThucHanh.Models;
+using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using X.PagedList;
+
+namespace _22521160_KiemTraThucHanh.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin")]
+    [Route("Admin/HomeAdmin")]
+    public class HomeAdminController : Controller
+    {
+        QlspContext db = new QlspContext();
+        [Route("")]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("QuanLySanPham")]
+        public IActionResult QuanLySanPham(int? page)
+        {
+            int pageSize = 12;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var lstSanpham = db.Products.AsNoTracking().OrderBy(x => x.Name);
+            PagedList<Product> lst = new PagedList<Product>(lstSanpham, pageNumber, pageSize);
+            return View(lst);
+        }
+    }
+}
